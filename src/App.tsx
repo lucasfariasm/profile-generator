31b2e10dbac40caf02b87de0bba92f203475908d
Lucas <lucasfarias.bsb@hotmail.com>
2021-03-10 17:44:12 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { CreateProfile } from './pages/CreateProfile';
+import { Footer } from './pages/Footer';
 import { Home } from './pages/Home';
 
 import './styles/global.css'
@@ -12,6 +13,7 @@ function App(){
     <>
       <Home/>
       <CreateProfile/>
+      <Footer/>
     </>
   );
 }
---
+++ src/pages/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/pages/Footer.module.css'
+
+export function Footer(){
+  return (
+    <footer>
+      <div className={styles.container}>
+        <p>Design e código por <span>Lucas Farias de Moura</span>💜</p> 
+      </div>
+    </footer>
+  )
+}
